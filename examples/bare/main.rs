d787535ce8ab64b1e8ee9ca101e10100ce62c138
eyzi <contact@eyzi.dev>
2022-10-29 23:48:22 -0400

    feat(bare): add camera

--- examples/bare/main.rs
+++ examples/bare/main.rs
@@ -1,7 +1,14 @@
 #![windows_subsystem = "windows"]
 
-use bevy::prelude::{App, DefaultPlugins};
+use bevy::prelude::{App, Camera2dBundle, Commands, DefaultPlugins};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_camera)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn().insert_bundle(Camera2dBundle::default());
 }
