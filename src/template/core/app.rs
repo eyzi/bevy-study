e3a5cddb3a88e3fa63446ccd602a73039aaa508c
eyzi <contact@eyzi.dev>
2022-11-12 22:19:01 -0500

    refactor(template): use mod.rs and refactor for 0.9

--- src/template/core/app.rs
+++ src/template/core/app.rs
@@ -6,15 +6,17 @@ use bevy::prelude::*;
 
 pub fn start() {
     App::new()
-        .insert_resource(WindowDescriptor {
-            width: config::WINDOW_WIDTH,
-            height: config::WINDOW_HEIGHT,
-            title: config::GAME_NAME.to_string(),
-            resizable: false,
-            present_mode: bevy::window::PresentMode::AutoVsync,
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: config::WINDOW_WIDTH,
+                height: config::WINDOW_HEIGHT,
+                title: config::GAME_NAME.to_string(),
+                resizable: false,
+                present_mode: bevy::window::PresentMode::AutoVsync,
+                ..default()
+            },
             ..default()
-        })
-        .add_plugins(DefaultPlugins)
+        }))
         .add_state(state::GameState::Playing)
         .add_startup_system(icon::setup)
         .add_startup_system(camera::setup)
